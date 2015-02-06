9f733ad3ec0f0d60dce9baa74a53ea0edd69b92c
Neurophobic <leosilverberg@gmail.com>
2015-02-06 15:51:19 +0000

    fix mongodb

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://admin:admin@ds037581.mongolab.com:37581/meanstack',
+	db: 'mongodb://admin:admin@ds037581.mongolab.com:37581/meanstack',
 	assets: {
 		lib: {
 			css: [
